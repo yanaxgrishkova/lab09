b9f11179a9c9d21e3537f284408b7816af4ae7a2
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-09-27 21:26:07 +0300

    Update print.cpp

--- sources/print.cpp
+++ sources/print.cpp
@@ -4,13 +4,6 @@ void print (const std::string& text, std::ostream& out) {
 out << text;
 }
 
-cat > sources/print.cpp << EOF
-#include <print.hpp>
-
-void print (const std::string& text, std::ostream& out) {
-out << text;
-}
-
 void print (const std::string& text, std::ofstream& out) {
 out << text;
 }
